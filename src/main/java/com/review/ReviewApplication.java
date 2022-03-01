9b10ac35c91e54f52c288932c66d7e44133a9337
Mahesh Khanvilkar <maheshkhanvilkar@Maheshs-MBP.attlocal.net>
2022-03-01 14:40:30 -0500

    enable swagger openAPI

--- src/main/java/com/review/ReviewApplication.java
+++ src/main/java/com/review/ReviewApplication.java
@@ -1,9 +1,12 @@
 package com.review;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Review API", version= "2.0", description = "Review Service APIs"))
 public class ReviewApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReviewApplication.class, args);
